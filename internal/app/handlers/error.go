c33df33a71a22bd3ba936833351cabda36d4ef8a
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-12-30 13:16:45 +0530

    updated error handler to specify by most specific at the top

--- internal/app/handlers/error.go
+++ internal/app/handlers/error.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/Akshay-Priyadarshi/fullstack-app/internal/app/models"
+	serviceerrors "github.com/Akshay-Priyadarshi/fullstack-app/internal/app/services/service_errors"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,12 +13,16 @@ func ErrorHandler(c *fiber.Ctx, err error) error {
 	if apiError, ok := err.(*models.ApiError); ok {
 		apiErrorServerResponse := apiError.GetApiResponse()
 		return c.Status(apiErrorServerResponse.StatusCode).JSON(apiErrorServerResponse)
-	}
-	if fiberError, ok := err.(*fiber.Error); ok {
+	} else if serviceError, ok := err.(serviceerrors.ServiceError); ok {
+		apiError := models.NewBadRequestError(serviceError.Error())
+		apiErrorServerResponse := apiError.GetApiResponse()
+		return c.Status(apiErrorServerResponse.StatusCode).JSON(apiErrorServerResponse)
+	} else if fiberError, ok := err.(*fiber.Error); ok {
 		fiberErrorServerResponse := models.NewApiResponse[any](fiberError.Message, fiberError.Code, nil, nil)
 		return c.Status(fiberErrorServerResponse.StatusCode).JSON(fiberErrorServerResponse)
+	} else {
+		defaultError := models.NewApiError(fiber.ErrInternalServerError.Message, fiber.StatusInternalServerError, nil)
+		defaultErrorApiResponse := defaultError.GetApiResponse()
+		return c.Status(defaultErrorApiResponse.StatusCode).JSON(defaultErrorApiResponse)
 	}
-	defaultError := models.NewApiError("Internal Server Error", fiber.StatusInternalServerError, nil)
-	defaultErrorApiResponse := defaultError.GetApiResponse()
-	return c.Status(defaultErrorApiResponse.StatusCode).JSON(defaultErrorApiResponse)
 }
