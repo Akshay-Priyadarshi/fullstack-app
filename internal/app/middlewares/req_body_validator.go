3e318bef160c5a8f1158e576e10a85beed3e7c15
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-12-30 13:16:22 +0530

    updated req body validator

--- internal/app/middlewares/req_body_validator.go
+++ internal/app/middlewares/req_body_validator.go
@@ -1,8 +1,13 @@
 package middlewares
 
 import (
+	"reflect"
+
 	"github.com/Akshay-Priyadarshi/fullstack-app/internal/app/dtos/abstractions"
 	"github.com/Akshay-Priyadarshi/fullstack-app/internal/app/models"
+	"github.com/Akshay-Priyadarshi/fullstack-app/internal/app/server"
+	serviceerrors "github.com/Akshay-Priyadarshi/fullstack-app/internal/app/services/service_errors"
+	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,11 +16,32 @@ func ReqBodyValidator[T any]() fiber.Handler {
 		var dto T
 		err := c.BodyParser(&dto)
 		if err != nil {
-			return models.New400ApiError("Invalid request body")
+			return models.NewBadRequestError("invalid request body")
 		}
 		if validatable, ok := any(&dto).(abstractions.Validatable); ok {
+			problemDetails := make(map[string]string)
 			if err := validatable.Validate(); err != nil {
-				return models.New400ApiError(err.Error())
+				if validationErrors, ok := err.(validator.ValidationErrors); ok {
+					dtoType := reflect.TypeOf(dto)
+					if dtoType.Kind() == reflect.Ptr {
+						dtoType = dtoType.Elem()
+					}
+					for _, fieldErr := range validationErrors {
+						fieldName := fieldErr.StructField()
+						invalidFieldName := fieldName
+						if dtoType.Kind() == reflect.Struct {
+							if field, found := dtoType.FieldByName(fieldName); found {
+								jsonTagValue := field.Tag.Get("json")
+								if jsonTagValue != "" && jsonTagValue != "-" {
+									invalidFieldName = jsonTagValue
+								}
+							}
+						}
+						problemDetails[invalidFieldName] = fieldErr.Translate(*server.AppServer.Translator)
+					}
+				}
+				validationApiError := models.NewValidationError(serviceerrors.ErrValidationFailed.Error(), problemDetails)
+				return validationApiError
 			}
 		}
 		c.Locals("validatedDto", &dto)
